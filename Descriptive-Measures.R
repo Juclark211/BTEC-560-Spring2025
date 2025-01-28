fa073bf83426541d076fc617b58c280c59fb09b5
Juclark211 <juclark@claflin.edu>
2025-01-28 10:14:01 -0500

    Add files via upload

---
+++ Descriptive-Measures.R
@@ -0,0 +1,23 @@
+#Author: Clark, Date: 01/28/25, Purpose: Descriptive measures functions
+
+#Calulate standard deviation
+#Create dummy dataset
+
+data <-c(1,2,3,4,5,6,7,8)
+data1 <-c("A", "B", "C")
+data2 <-c(1:1000)
+
+sd(data)
+
+sd(data)
+[1] 2.44949
+sd(data2)
+[1] 288.8194
+
+#Calculate mean
+mean(data)
+
+#Calculate median
+median(data)
+
+#Calculate mode in R
